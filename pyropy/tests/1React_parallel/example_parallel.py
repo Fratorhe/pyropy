1eefe27fbc85cedb8ab38a094d5a85e3041dfaf8
Jérémy Chevalier <jeremy.chevalier@u-bordeaux.fr>
2022-07-19 15:36:29 +0200

    Update example_parallel.py to plot numerical and analytical solutions

--- pyropy/tests/1React_parallel/example_parallel.py
+++ pyropy/tests/1React_parallel/example_parallel.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as colormaps
+cmap = plt.cm.Dark2.colors
 
 from pyropy import ReactManager, PyrolysisParallel
 
@@ -12,16 +14,36 @@ test = PyrolysisParallel(temp_0=273, temp_end=2000, beta=beta, n_points=200, rea
 
 # Numerical solution using 200 points
 test.solve_system()
-
 rho = test.rho_solid
+drho = test.drho_solid
 t = test.time
 T = test.temperature
-plt.plot(T, rho)
-plt.show()
 test.to_csv(f'test_{beta}Kmin.csv')
+plt.figure(1)
+plt.plot(T, rho,label='Numerical')
+plt.figure(2)
+plt.plot(T, drho,label='Numerical')
 
-# Analytical solution using 25 points
-#betaKstest = beta/60
-#test.compute_analytical_solution()
+# Analytical solution
+test.compute_analytical_solution()
+rho = test.rho_solid
+drho = test.drho_solid
+t = test.time
+T = test.temperature
+plt.figure(1)
+plt.plot(T, rho,label='Analytical')
+plt.figure(2)
+plt.plot(T, drho,label='Analytical')
 
-#test.compute_time(betaKs=betaKstest,temp_0=273, temp_end=2000, n_points=25)
+
+# Create the legend
+plt.figure(1)
+plt.title('Mass loss')
+plt.legend()
+
+plt.figure(2)
+plt.title('Mass loss rate')
+plt.legend()
+
+
+plt.show()
