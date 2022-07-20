a8581142de9f24bbb34f2b80562bda3bb3e553c8
Fran <fratorhe@vki.ac.be>
2022-07-20 09:55:10 +0200

    re-factored analytical solution as a new class

--- pyropy/tests/1React_parallel/example_parallel.py
+++ pyropy/tests/1React_parallel/example_parallel.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as colormaps
 cmap = plt.cm.Dark2.colors
 
-from pyropy import ReactManager, PyrolysisParallel
+from pyropy import ReactManager, PyrolysisParallel, PyrolysisParallelAnalytical
 
 reactions = ReactManager("data_parallel_verification.json")
 reactions.react_reader()
 reactions.param_reader()
 
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
 
 beta = 5
 test = PyrolysisParallel(temp_0=273, temp_end=2000, beta=beta, n_points=200, reaction_scheme_obj=reactions)
@@ -19,31 +21,26 @@ drho = test.drho_solid
 t = test.time
 T = test.temperature
 test.to_csv(f'test_{beta}Kmin.csv')
-plt.figure(1)
-plt.plot(T, rho,label='Numerical')
-plt.figure(2)
-plt.plot(T, drho,label='Numerical')
+ax1.plot(T, rho,label='Numerical')
+ax2.plot(T, drho,label='Numerical')
 
 # Analytical solution
-test.compute_analytical_solution()
+test = PyrolysisParallelAnalytical(temp_0=273, temp_end=2000, beta=beta, n_points=200, reaction_scheme_obj=reactions)
+test.solve_system()
 rho = test.rho_solid
 drho = test.drho_solid
 t = test.time
 T = test.temperature
-plt.figure(1)
-plt.plot(T, rho,label='Analytical')
-plt.figure(2)
-plt.plot(T, drho,label='Analytical')
+ax1.plot(T, rho,label='Analytical')
+ax2.plot(T, drho,label='Analytical')
 
 
 # Create the legend
-plt.figure(1)
-plt.title('Mass loss')
-plt.legend()
+ax1.set_title('Mass loss')
+ax1.legend()
 
-plt.figure(2)
-plt.title('Mass loss rate')
-plt.legend()
+ax2.set_title('Mass loss rate')
+ax2.legend()
 
 
 plt.show()
