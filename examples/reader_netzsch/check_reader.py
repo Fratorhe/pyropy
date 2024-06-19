c25215de2984d2fa8699cebe1e5e1f9565c136e4
Fran <fratorhe@vki.ac.be>
2024-06-18 21:22:00 -0500

    added part example on partial function application

--- examples/reader_netzsch/check_reader.py
+++ examples/reader_netzsch/check_reader.py
@@ -11,3 +11,21 @@ netzsch = ExperimentReaderNetzsch(
 netzsch.plotit()
 
 plt.show()
+
+
+# since the original Protocol for file reader only wants the filename and the folder, we can do a partial application of the filter
+
+from functools import partial
+
+# Create a partial function that always sets savgol_poly_order=savgol_poly and savgol_win_length=savgol_win
+ExperimentReaderNetzschSavgol = partial(
+    ExperimentReaderNetzsch, savgol_poly_order=savgol_poly, savgol_win_length=savgol_win
+)
+
+netzsch2 = ExperimentReaderNetzsch(
+    "ExpDat_VKI_P50_20K_50ml+20ml_01.txt", savgol_poly_order=savgol_poly, savgol_win_length=savgol_win
+)
+
+netzsch2.plotit()
+
+plt.show()
